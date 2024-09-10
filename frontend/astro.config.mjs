63a08e8324b675d75ef4839b4f9a9c4af678777b
felipetodev <fe.ossandon.u@gmail.com>
2024-09-10 20:53:37 -0300

    feat(front): add events

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -9,5 +9,7 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
