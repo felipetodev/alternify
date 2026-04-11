c773a6bfcf4f7664bb035b784f3aed845e715d2b
felipetodev <fe.ossandon.u@gmail.com>
2026-04-11 00:51:25 -0400

    chore: update deps

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
--- frontend/src/pages/api/share.ts
+++ frontend/src/pages/api/share.ts
@@ -1,16 +1,16 @@
 import type { APIRoute } from "astro";
-import { insertShare } from "../../../db/turso";
+import { insertShare } from "@db/turso";
 import { validateShareRequest } from "@/lib/schemas";
 
 export const POST: APIRoute = async ({ request }) => {
-  const { id, url, image = '' } = await request.json()
+  const { id, url, image = "" } = await request.json();
 
-  const params = new URLSearchParams(url)
-  const result = validateShareRequest(id, params)
+  const params = new URLSearchParams(url);
+  const result = validateShareRequest(id, params);
 
   if (!result.success) {
-    console.error(result.error.errors)
-    return new Response('Invalid credentials', { status: 400 })
+    console.error(result.error.errors);
+    return new Response("Invalid credentials", { status: 400 });
   }
 
   try {
@@ -18,11 +18,11 @@ export const POST: APIRoute = async ({ request }) => {
       id,
       url,
       image,
-    })
+    });
 
-    return new Response('', { status: 200 })
+    return new Response("", { status: 200 });
   } catch (e) {
-    console.error(e)
-    return new Response('Error generating social url', { status: 500 })
+    console.error(e);
+    return new Response("Error generating social url", { status: 500 });
   }
-}
+};
